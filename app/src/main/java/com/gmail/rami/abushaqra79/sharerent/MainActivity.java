baf96cc33de8da398307158a9038547de57615f6
RamiAbuShaqra <RamiAbuShaqra@users.noreply.github.com>
2021-09-11 03:24:20 +0300

    Add views to activity_main.xml file

--- app/src/main/java/com/gmail/rami/abushaqra79/sharerent/MainActivity.java
+++ app/src/main/java/com/gmail/rami/abushaqra79/sharerent/MainActivity.java
@@ -3,12 +3,25 @@ package com.gmail.rami.abushaqra79.sharerent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String[] COUNTRIES = new String[] {
+            "Belgium", "France", "Italy", "Germany", "Spain"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+        AutoCompleteTextView textView = findViewById(R.id.locations);
+        textView.setAdapter(adapter);
+
+        textView.setThreshold(2);
     }
 }
