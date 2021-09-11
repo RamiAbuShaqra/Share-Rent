25a3bcf9290e2babd6678b0458a17025577d718c
RamiAbuShaqra <RamiAbuShaqra@users.noreply.github.com>
2021-09-12 01:18:41 +0300

    Add location and dates entries

--- app/src/main/java/com/gmail/rami/abushaqra79/sharerent/MainActivity.java
+++ app/src/main/java/com/gmail/rami/abushaqra79/sharerent/MainActivity.java
@@ -2,9 +2,19 @@ package com.gmail.rami.abushaqra79.sharerent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,16 +22,108 @@ public class MainActivity extends AppCompatActivity {
             "Belgium", "France", "Italy", "Germany", "Spain"
     };
 
+    private final long EXTRA_DAY = 86400000; // 86,400,000 millisecond in one day.
+    private Calendar myCalendar;
+    private EditText startRentalDate;
+    private EditText endRentalDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_dropdown_item_1line, COUNTRIES);
         AutoCompleteTextView textView = findViewById(R.id.locations);
         textView.setAdapter(adapter);
 
         textView.setThreshold(2);
+
+
+
+
+
+
+        myCalendar = Calendar.getInstance();
+
+        startRentalDate = findViewById(R.id.start_date);
+        endRentalDate = findViewById(R.id.end_date);
+
+        DatePickerDialog.OnDateSetListener firstDate = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                startRentalDate.setText(updateLabel());
+            }
+        };
+
+        DatePickerDialog.OnDateSetListener secondDate = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                endRentalDate.setText(updateLabel());
+            }
+        };
+
+        startRentalDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePicker = new DatePickerDialog(MainActivity.this,
+                        firstDate, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH));
+
+                // Set today's date as minimum date and all the past dates are disabled
+                datePicker.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
+                datePicker.show();
+            }
+        });
+
+        endRentalDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePicker = new DatePickerDialog(MainActivity.this,
+                        secondDate, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH));
+
+                if (getStartDate() == 0) {
+                    datePicker.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
+                } else {
+                    datePicker.getDatePicker().setMinDate(getStartDate());
+                }
+
+                datePicker.show();
+            }
+        });
+    }
+
+    private String updateLabel() {
+        String dateFormat = "dd/MM/yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat, Locale.ROOT);
+
+        return simpleDateFormat.format(myCalendar.getTime());
+
+    }
+
+    private long getStartDate() {
+        long time = 0;
+        String startDate = startRentalDate.getText().toString();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.ROOT);
+
+        try {
+            Date date = simpleDateFormat.parse(startDate);
+            if (date != null) {
+                time = date.getTime() + EXTRA_DAY;
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return time;
     }
 }
