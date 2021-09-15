3626fa05e209932c34b542d02ba2610b90019ed6
RamiAbuShaqra <RamiAbuShaqra@users.noreply.github.com>
2021-09-16 01:31:21 +0300

    Create intent to open LoginActivity

--- app/src/main/java/com/gmail/rami/abushaqra79/sharerent/SignupActivity.java
+++ app/src/main/java/com/gmail/rami/abushaqra79/sharerent/SignupActivity.java
@@ -2,13 +2,27 @@ package com.gmail.rami.abushaqra79.sharerent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private TextView signInPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        signInPage = findViewById(R.id.sign_in_page);
+        signInPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
