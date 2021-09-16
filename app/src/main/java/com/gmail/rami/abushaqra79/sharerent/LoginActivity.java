c32ec18528de326452aae61f208773b9e4d8b766
RamiAbuShaqra <RamiAbuShaqra@users.noreply.github.com>
2021-09-16 03:07:36 +0300

    Update Login Activity

--- app/src/main/java/com/gmail/rami/abushaqra79/sharerent/LoginActivity.java
+++ app/src/main/java/com/gmail/rami/abushaqra79/sharerent/LoginActivity.java
@@ -2,13 +2,27 @@ package com.gmail.rami.abushaqra79.sharerent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView signUpPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signUpPage = findViewById(R.id.sign_up_page);
+        signUpPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
