6865492bc7e2a5a9eafd7b50a87abf383ce61960
RamiAbuShaqra <RamiAbuShaqra@users.noreply.github.com>
2021-09-17 03:14:57 +0300

    Add code to create new user in Sign up activity

--- app/src/main/java/com/gmail/rami/abushaqra79/sharerent/SignupActivity.java
+++ app/src/main/java/com/gmail/rami/abushaqra79/sharerent/SignupActivity.java
@@ -1,20 +1,96 @@
 package com.gmail.rami.abushaqra79.sharerent;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
 
     private TextView signInPage;
+    private EditText email;
+    private EditText password;
+    private EditText confirmPassword;
+    private Button register;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        confirmPassword = findViewById(R.id.confirm_password);
+
+        register = findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String enteredEmail = email.getText().toString().trim();
+                String enteredPassword = password.getText().toString().trim();
+                String confirmedPassword = confirmPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(enteredEmail)) {
+                    email.setError("Enter a valid email address");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(enteredPassword)) {
+                    password.setError("Enter password");
+                    return;
+                }
+
+                if (enteredPassword.length() < 8) {
+                    password.setError("Password should be minimum 8 characters");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(confirmedPassword)) {
+                    confirmPassword.setError("Enter the password again");
+                    return;
+                }
+
+                if (!enteredPassword.equals(confirmedPassword)) {
+                    confirmPassword.setError("Passwords are not match");
+                    return;
+                }
+
+                firebaseAuth.createUserWithEmailAndPassword(enteredEmail, enteredPassword)
+                .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast toast = Toast.makeText(SignupActivity.this,
+                                    "Authentication failed! Couldn't create account." +
+                                            " Please try again.", Toast.LENGTH_SHORT);
+                            toast.show();
+                        } else {
+                            /**
+                             * 1) Start the complete profile activity.
+                             * 2) You may need to add a progress bar.
+                             * 3) Add a touch feel to the button or change it to text view.
+                             * 4) Check if the email is already registered and display a message.
+                             */
+                        }
+                    }
+                });
+            }
+        });
 
         signInPage = findViewById(R.id.sign_in_page);
         signInPage.setOnClickListener(new View.OnClickListener() {
