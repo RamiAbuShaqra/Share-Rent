151070eb04832cc623fb625db968f41219209b5f
RamiAbuShaqra <RamiAbuShaqra@users.noreply.github.com>
2021-10-09 20:33:57 +0300

    Add listview to display list of results

--- app/src/main/java/com/gmail/rami/abushaqra79/sharerent/ReviewItemsOptions.java
+++ app/src/main/java/com/gmail/rami/abushaqra79/sharerent/ReviewItemsOptions.java
@@ -3,12 +3,30 @@ package com.gmail.rami.abushaqra79.sharerent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ReviewItemsOptions extends AppCompatActivity {
+
+    // TODO add code to return back to previous version of ChooseItemsActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review_items_options);
+
+        Bundle bundle = getIntent().getExtras();
+        String destination = bundle.getString("Destination");
+        String startDate = bundle.getString("Start Date");
+        String endDate = bundle.getString("End Date");
+        ArrayList<String> items = bundle.getStringArrayList("Selected Items");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.baby_gear_details,
+                R.id.item_description, items);
+
+        ListView listView = findViewById(R.id.review_items_list);
+        listView.setAdapter(adapter);
     }
 }
